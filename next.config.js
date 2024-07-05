f5c5a16c2e4b93584b682290927c3fecfc00475e
unknown <shukhrat.nazarkhudoev@gmail.com>
2024-07-05 16:03:02 +0500

    installed sharpy

--- next.config.js
+++ next.config.js
@@ -1,9 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: { formats: ['image/webp'] }
-}
-// const nextConfig = {
-//   images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
-// };
-
-module.exports = nextConfig;
+    images: {
+      formats: ['image/webp'],
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "**"
+        }
+      ]
+    }
+  };
+  
+  module.exports = nextConfig;
