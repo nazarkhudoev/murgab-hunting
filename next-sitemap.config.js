dceb3edc0696dd16f354a82bed25b0dd2cb90a08
unknown <shukhrat.nazarkhudoev@gmail.com>
2024-07-15 15:14:17 +0500

    sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://murgabhunting.com',
+    generateRobotsTxt: true,
+    sitemapSize: 7000, // (optional)
+    // ...other options
+};
+
