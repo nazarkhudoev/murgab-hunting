b67180726c0d95b0487e76d3e120e4c20c5015aa
unknown <shukhrat.nazarkhudoev@gmail.com>
2024-07-23 02:06:23 +0500

    og image added

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
+
 import { Inter, Roboto } from "next/font/google";
 import Head from "next/head";
+import ogImage from "../../../public/og-image.png";
 
 import "./globals.css";
 
@@ -9,6 +11,7 @@ import { Locale, i18n } from "@/i18n.config";
 // Components
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import { Params } from "next/dist/shared/lib/router/utils/route-matcher";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,10 +21,15 @@ const roboto = Roboto({
   subsets: ["latin"],
 });
 
-export const metadata: Metadata = {
-  title: "Murgab Hunting",
-  description: "Murgab Hunting",
+export async function generateMetadata({ params: { lang } }: Params): Promise<Metadata> {
+
+  return {
+    title: "Murgab Hunting",
+    description: "Murgab Hunting",
+  }
+
 };
+
 
 export async function generateStaticParams() {
   return i18n.locales.map((locale) => ({ lang: locale }));
@@ -36,6 +44,14 @@ export default function RootLayout({
 }) {
   return (
     <html className={roboto.className} lang={params.lang}>
+       <Head>
+        <title>Murgab Hunting</title>
+        <meta name="description" content="Murgab Hunting offers the ultimate adventure in Tajikistan's remote wilderness (Pamir). Since 1989, we've specialized in bespoke sport hunting expeditions in the majestic Pamir Gornobadakhshan Autonomous Region. " />
+        <meta property="og:title" content="Murgab Hunting" />
+        <meta property="og:description" content="Murgab Hunting offers unparalleled hunting adventures in Tajikistan's remote wilderness (Pamir). Founded in 1989, we provide bespoke expeditions in Pamir (the Gornobadakhshan Autonomous Region, led by a team of seasoned explorers and professional hunters.)" />
+        <meta property="og:image" content={ogImage.src} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
       <body>
         <Header lang={params.lang} />
         {children}
